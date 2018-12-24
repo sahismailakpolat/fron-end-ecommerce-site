4071c330d066ed497c158e6cbeac91a7431fc6ff
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-24 08:15:27 -0600

    developed navbar and header actions for setting new links and used in account.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
+
 import headerNavbar from './headernavbarReducer';
+
 const rootReducer = combineReducers({
   form,
   headerNavbar
