bcad7941b8bad62e22a8e7bf7a1759d8fb9fb66c
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-25 07:15:19 -0600

    built user reducer with fetch purchases action creatorto fetch user data

---
+++ src/components/account/purchases.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Purchases extends Component {
+
+    componentDidMount() {
+        this.props.fetchUserPurchases();
+    }
+
+    render() {
+        const { className } = this.props;
+
+        return (
+            <div className={`${className} purchases`}>
+                {
+                    this.props.purchases.map(purchase => {
+                        return <div>{purchase.title}</div>
+                    })
+                }
+            </div>
+
+
+        )
+    }
+
+}
+
+function mapStateToProps(state) {
+    const { purchases } = state.user;
+    return { purchases };
+}
+
+Purchases = connect(mapStateToProps, actions)(Purchases);
+export default Purchases;
