b6e12a615650d9b9807bb139b62233a0baf1822f
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-26 14:53:13 -0600

    finished account information form

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -80,7 +80,21 @@ class AccountInformationForm extends Component {
                                 title='Confirm Password'
                                 placeholder='Confirm Password'
                                 name='confirm'
-                                component={FormInput} />
+                                component={FormInput} />,
+                            <div key={3} className='account-information-form__line'></div>,
+                            <Field key={4} className='account-information-form__update-information'
+                                onClick={() => this.setState({ showPasswords: false })}
+                                type='submit'
+                                title='Update Information'
+                                name='update-information'
+                                component={FormButton} />,
+                            <Field key={5} className='account-information-form__cancel'
+                                onClick={() => this.setState({ showPasswords: false })}
+                                type='button'
+                                title='Cancel'
+                                name='cancel'
+                                short={true}
+                                component={FormButton} />
                         ]
                         :
                         <Field className='account-information-form__change-password'
