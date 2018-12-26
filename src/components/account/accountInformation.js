911c082a29ab6268e8f9d2f4673b2dcdea685d58
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-26 10:45:11 -0600

    built account information grid and placed majority of fields on form grid

--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import PageTitle from '../pageTitle';
 import AccountInformationForm from './accountInformationForm';
+
 class AccountInformation extends Component {
-    
+
     onSubmit = (fields) => {
         console.log(fields);
     }
-    
+
     render() {
         return (
             <div className='account-information'>
                 <PageTitle className='account-information__title' title='Account Information' />
                 <AccountInformationForm onSubmit={this.onSubmit} className='account-information__form' />
-            
-                </div>
+
+            </div>
         )
     }
 }
--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -9,48 +9,51 @@ import history from '../../history';
 class AccountInformationForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
-       
+
         return (
-            <form onSubmit={handleSubmit} className={`${className} sign-up-form`}>
-                <Field className='sign-up-form__name'
+            <form onSubmit={handleSubmit} className={`${className} account-information-form`}>
+                <Field className='account-information-form__name'
                     type='name'
                     title='Name'
                     placeholder='Name'
                     name='name'
                     component={FormInput} />
-                <Field className='sign-up-form__email'
+                <Field className='account-information-form__email'
                     type='email'
                     title='Email'
                     placeholder='Email'
                     name='email'
                     component={FormInput} />
-                <Field className='sign-up-form__password'
-                    type='password'
-                    title='Password'
-                    placeholder='Password'
-                    name='password'
+
+
+                <Field className='account-information-form__street address'
+                    type='address'
+                    title='Street Address'
+                    placeholder='Street Address'
+                    name='address'
                     component={FormInput} />
-                <Field className='sign-up-form__confirm'
-                    type='password'
-                    title='Confirm Password'
-                    placeholder='Confirm Password'
-                    name='Confirm'
+                <Field className='account-information-form__city'
+                    type='city'
+                    title='City'
+                    placeholder='City'
+                    name='city'
                     component={FormInput} />
 
-                <div className='sign-up-form__line'></div>
-                <Field className='sign-up-form__login'
-                    onClick={() => history.push('./account')}
-                    type='submit'
-                    title='Create Account'
-                    name='login'
-                    component={FormButton} />
-                <Field className='sign-up-form__back'
-                    onClick={() => history.push('./signin')}
-                    type='button'
-                    title='Back'
-                    name='back'
-                    short={true}
-                    component={FormButton} />
+                <Field className='account-information-form__state'
+                    type='state'
+                    title='State'
+                    placeholder='State'
+                    name='state'
+                    component={FormInput} />
+                <Field className='account-information-form__zipcode'
+                    type='zipcode'
+                    title='Zipcode'
+                    placeholder='Zipcode'
+                    name='zipcode'
+                    component={FormInput} />
+
+
+
             </form>
         )
     }
@@ -60,5 +63,4 @@ AccountInformationForm = reduxForm({
     form: 'AccountInformationForm'
 })(AccountInformationForm);
 
-
-export default AccountInformationForm;
+export default AccountInformationForm; 
