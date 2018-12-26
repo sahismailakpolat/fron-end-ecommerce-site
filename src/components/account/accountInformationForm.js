f930d4230bafc31f52e9c04b7bae44f0c18a1299
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-26 13:58:45 -0600

    added password fields

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 
 import { reduxForm, Field } from 'redux-form';
 
-import { FormInput, FormButton } from '../formFields';
+import { FormInput, FormButton, LongGrayButton } from '../formFields';
 
 import history from '../../history';
 
 class AccountInformationForm extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            showPasswords: false
+        }
+    }
     render() {
         const { className, handleSubmit } = this.props;
 
@@ -51,6 +59,40 @@ class AccountInformationForm extends Component {
                     placeholder='Zipcode'
                     name='zipcode'
                     component={FormInput} />
+
+                {
+                    this.state.showPasswords ?
+                        [
+                            <Field key={0} className='account-information-form__current'
+                                type='password'
+                                title='Current Password'
+                                placeholder='Current Password'
+                                name='current'
+                                component={FormInput} />,
+                            <Field key={1} className='account-information-form__new'
+                                type='password'
+                                title='New Password'
+                                placeholder='New Password'
+                                name='new'
+                                component={FormInput} />,
+                            <Field key={2} className='account-information-form__confirm'
+                                type='password'
+                                title='Confirm Password'
+                                placeholder='Confirm Password'
+                                name='confirm'
+                                component={FormInput} />
+                        ]
+                        :
+                        <Field className='account-information-form__change-password'
+                            onClick={() => this.setState({ showPasswords: true })}
+                            type='button'
+                            labelTitle='Password'
+                            title='Change Password'
+                            name='change-password'
+                            component={LongGrayButton} />
+                }
+
+
 
 
 
--- src/components/formFields.js
+++ src/components/formFields.js
@@ -29,3 +29,23 @@ export class FormButton extends Component {
         )
     }
 }
+
+export class LongGrayButton extends Component {
+    render() {
+        const { className, title, labelTitle, type, input, onClick } = this.props;
+        return (
+            <div className={`${className} form-button-long-gray`}>
+                <label className='form-button-long-gray__label'>{labelTitle}</label>
+
+                <button className={`form-button-long-gray__button`}
+                    type={type}
+                    {...input}
+                    onClick={onClick}>
+                    {title}
+                </button>
+
+            </div >
+
+        )
+    }
+}
