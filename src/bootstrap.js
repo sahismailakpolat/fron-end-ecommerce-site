738285ef2898db3fa94257e927a51326d121a445
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-27 02:05:57 -0600

    built base for shop component, built route etc

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -16,7 +16,7 @@ import Layout from './components/layout';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
 import Account from './components/account/account';
-
+import Shop from './components/shop/shop';
 
 function main() {
   ReactDOM.render(
@@ -29,6 +29,8 @@ function main() {
             <Route path='/signup' exact component={Signup} />
 
             <Route path='/account' exact component={Account} />
+
+            <Route path='/shop' exact component={Shop} />
 
           </Switch>
         </Layout>
---
+++ src/components/shop/shop.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Shop extends Component {
+
+    componentDidMount() {
+        const headerLinks = [
+            {
+                _id: 0,
+                title: 'Login',
+                path: '/signin'
+            }
+
+        ]
+        this.props.setHeaderLinks(headerLinks);
+
+    }
+
+    render() {
+        return (
+            <div className='shop'>
+                shop....
+        </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return { state }
+}
+
+Shop = connect(mapStateToProps, actions)(Shop);
+
+export default Shop;
