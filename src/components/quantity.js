1739926ee251b2ba56db0943abbedee44a9df44b
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-27 15:02:34 -0600

    built green price tag component and quantity component and added remaining jsx to product component

---
+++ src/components/quantity.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Quantity extends Component {
+    render() {
+        const { className, quantity } = this.props;
+        return (
+            <div className={`${className} quantity`}>
+                <div className='quantity__count'>
+                    {quantity}
+                </div>
+
+                <div className='quantity__plus'>
+                    <i className='fas fa-plus'></i>
+                </div>
+                <div className='quantity_minus'>
+                    <i className='fas fa-minus'></i>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Quantity;
