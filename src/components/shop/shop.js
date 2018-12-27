29d5a08a9847c5bb47adf5ae3ba1aa13109d6105
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-27 08:34:21 -0600

    refactored navbar to take additional onClick

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -16,7 +16,15 @@ class Shop extends Component {
         ]
         this.props.setHeaderLinks(headerLinks);
         this.props.fetchShopCategories();
+
         this.props.fetchShopProducts();
+    }
+
+    shouldComponentUpdate(nextProps) {
+        if (this.props != nextProps) {
+            this.props.setNavbarLinks(nextProps.categories, (_id) => this.props.filterProductsWithCategoryId(_id));
+        }
+        return true
     }
 
     render() {
@@ -29,7 +37,10 @@ class Shop extends Component {
 }
 
 function mapStateToProps(state) {
-    return { state }
+    const { categories } = state.shop;
+    return {
+        categories
+    }
 }
 
 Shop = connect(mapStateToProps, actions)(Shop);
