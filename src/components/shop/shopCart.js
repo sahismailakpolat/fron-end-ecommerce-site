676c541a50e67d94c09d67012d1492229efc95ec
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-29 05:32:40 -0600

    structured cart content products component and styled title

---
+++ src/components/shop/shopCart.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+function CartButton({ className, icon }) {
+    return (
+        <div className={`${className} cart-button`}>
+            <i className={icon} />
+        </div>
+    )
+}
+
+function CartContent({ className, products }) {
+    let count = products.length;
+    return (
+        <div className={`${className} cart-content`}>
+            <div className='card-content__title'>
+                Cart ({count})
+            </div>
+            <div className='cart-content__products'>
+
+            </div>
+            <div className='cart-content__footer'>
+
+            </div>
+        </div>
+    )
+}
+
+class ShopCart extends Component {
+    render() {
+        const { className } = this.props;
+        return (
+            <div className={`${className} shop-cart`}>
+
+                <CartButton className='shop-cart__toggle' icon='fas fa-times' />
+                <CartContent className='shop-cart__content' products={[]} />
+            </div>
+        )
+    }
+}
+export default ShopCart;
